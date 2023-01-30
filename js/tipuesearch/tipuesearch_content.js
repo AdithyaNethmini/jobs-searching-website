2f4b3529ed9d3d3a62a5a0db88b7b79a7482d7cc
AdithyaNethmini <adithya.kotuwege@gmail.com>
2023-01-30 13:48:25 +0530

    Initial commit

---
+++ js/tipuesearch/tipuesearch_content.js
@@ -0,0 +1,46 @@
+
+var tipuesearch = {"pages": [
+     {"title": "Agriculture", "text": "Agriculture job page", "url": "./agriculture.html"},
+
+     {"title": "Agri", "text": "Agriculture job page", "url": "./agriculture.html"},
+
+     {"title": "Apparel", "text": "Apparel job page", "url": "./apparel.html"},
+
+     {"title": "Beauty", "text": "Beauty culture job page", "url": "./beauty.html"},
+
+     {"title": "Culture", "text": "Beauty culture job page", "url": "./beauty.html"},
+
+     {"title": "Beauty Culture", "text": "Beauty culture job page", "url": "./beauty.html"},
+
+     {"title": "Constructions", "text": "Constructions job page", "url": "./constructios.html"},
+
+     {"title": "Education", "text": "Education job page", "url": "./education.html"},
+
+     {"title": "Finance", "text": "Financial job page", "url": "./financial.html"},
+
+     {"title": "Health", "text": "Health job page", "url": "./health.html"},
+
+     {"title": "Hotels", "text": "Hotels and Tourism job page", "url": "./hotels.html"},
+
+     {"title": "Tourism", "text": "Hotels and Tourism job page", "url": "./hotels.html"},
+
+     {"title": "Hotels and Tourism", "text": "Hotels and Tourism job page", "url": "./hotels.html"},
+
+     {"title": "Information Technology", "text": "Information Technology job page", "url": "./it.html"},
+
+     {"title": "Information", "text": "Information Technology job page", "url": "./it.html"},
+
+     {"title": "Technology", "text": "Information Technology job page", "url": "./it.html"},
+
+     {"title": "it", "text": "Information Technology job page", "url": "./it.html"},
+
+     {"title": "Management", "text": "Management job page", "url": "./management.html"},
+
+     {"title": "Other", "text": "Other jobs page", "url": "./other.html"},
+
+     {"title": "About", "text": "About Page", "url": "./about.html"},
+
+     {"title": "Contact", "text": "Contact Page", "url": "./contact.html"},
+
+     {"title": "Home", "text": "Home Page", "url": "./index.html"}
+]};
